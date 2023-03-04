1cf8cbccacd086c8cc7a3e9461d471f011ee9159
viper-black <94998319+viper-black@users.noreply.github.com>
2023-03-04 12:54:40 +0100

    Added planet rotation

--- Assets/Scripts/rotateAround.cs
+++ Assets/Scripts/rotateAround.cs
@@ -6,21 +6,18 @@ public class rotateAround : MonoBehaviour
 {
     [SerializeField] float rotationSpeed;
     [SerializeField] Vector3 pivotAngle;
-    [SerializeField] GameObject planet;
+    [SerializeField] GameObject Center;
     float distance;
     // Start is called before the first frame update
     void Start()
     {
-        distance = 2000f;
     }
 
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 Pivotposition = planet.transform.position;
+        Vector3 Pivotposition = Center.transform.position;
         this.transform.RotateAround(Pivotposition, pivotAngle, rotationSpeed * Time.deltaTime);
-
-        transform.position = (transform.position - planet.transform.position).normalized * distance + planet.transform.position;
     }
 }
